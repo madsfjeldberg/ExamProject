9f562ff9387a70bfc5b2a7c9283a29fc7bb3960a
madsfjeldberg <mafj0001@stud.kea.dk>
2024-04-26 11:23:40 +0200

    init commit

---
+++ src/main/java/dev/examproject/ExamProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.examproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/examproject/ExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.examproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
