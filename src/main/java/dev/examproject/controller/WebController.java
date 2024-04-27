ac64840314f257d3315a2fc2fb465812ad55d99f
madsfjeldberg <mafj0001@stud.kea.dk>
2024-04-27 12:07:59 +0200

    basic homepage added.

--- src/main/java/dev/examproject/controller/WebController.java
+++ src/main/java/dev/examproject/controller/WebController.java
@@ -1,12 +1,22 @@
 package dev.examproject.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "")
 public class WebController {
 
+    @GetMapping
+    public String redirectHome() {
+        return "redirect:/home";
+    }
 
-
+    @GetMapping(path = "home")
+    public String home(Model model, HttpSession session) {
+        return "home";
+    }
 }
