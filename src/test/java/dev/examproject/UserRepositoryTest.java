ff4524bba1228b726b9b5bd55bfc15c146fa9bfb
madsfjeldberg <mafj0001@stud.kea.dk>
2024-05-06 14:06:57 +0200

    tests til UserRepository og ProjectRepository færdig.

--- src/test/java/dev/examproject/UserRepositoryTest.java
+++ src/test/java/dev/examproject/UserRepositoryTest.java
@@ -2,13 +2,10 @@ package dev.examproject;
 
 import dev.examproject.model.User;
 import dev.examproject.repository.UserRepository;
-import dev.examproject.repository.util.ConnectionManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
