dc0a29d77a7ecf01fe2da5ff27925209a09d7349
moha3052 <moha3052@hotmail.dk>
2024-05-17 14:48:32 +0200

    Ny custom error. Oversæt til dansk og min integrationstest

--- src/test/java/dev/examproject/TaskRepositoryTest.java
+++ src/test/java/dev/examproject/TaskRepositoryTest.java
@@ -2,10 +2,14 @@ package dev.examproject;
 
 import dev.examproject.model.Task;
 import dev.examproject.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,18 +18,32 @@ public class TaskRepositoryTest {
 
     @Autowired
     private TaskRepository taskRepository;
-
+    @BeforeEach
+    void addTestTask(){
+        Task task = new Task(1, "user", "new user", 12);
+        taskRepository.addTask(task);
+    }
     @Test
     void addTask(){
-       var task = new Task(1, "user", "new user", 12);
-       var id = taskRepository.addTask(task);
-       assertNotEquals(-1, id);
+        Task task = taskRepository.getTask(1);
+       assertNotEquals(-1, task.getTaskId());
     }
 
     @Test
     void getProjectTasks(){
-        var projectTask = taskRepository.getProjectTasks(1);
-       assertNotEquals(0, projectTask);
+        List<Task> projectTask = taskRepository.getProjectTasks(1);
+       assertNotEquals(0, projectTask.size());
+    }
+
+    @Test
+    void getTotalRequiredHoursForSubproject(){
+        int taskSize = taskRepository.getTotalRequiredHoursForSubproject(1);
+        assertEquals(28, taskSize);
+    }
+
+    @Test
+    void getAssignedUsers(){
+
     }
 
 
